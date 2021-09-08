3dad176875076ebd9d3d1285530bb34b040ad311
hqian5 <haoqian721@163.com>
2021-09-09 00:20:51 +0800

    head first java - P24

---
+++ JavaLearn1.java
@@ -0,0 +1,29 @@
+package stockViewer;
+
+/**
+ * Created By HaoQian on 2021/9/8
+ */
+public class JavaLearn1 {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 4) {
+            System.out.print("a");
+            if (x < 1) {
+                System.out.print(" ");
+            }
+            System.out.print("n");
+            if (x > 1) {
+                System.out.print(" oyster");
+                x = x + 2;
+            }
+            if (x == 1) {
+                System.out.print("noys");
+            }
+            if (x < 1) {
+                System.out.print("noise");
+            }
+            System.out.println();
+            x = x + 1;
+        }
+    }
+}
